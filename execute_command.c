58a130eea82ac210e627445901a4b0bd86224c7b
Calvin Sewornu <calvinsewornu123@gmail.com>
2023-11-15 15:20:31 -0800

    command seperator

--- execute_command.c
+++ execute_command.c
@@ -3,38 +3,48 @@
 /**
  * execute_command - a function that executes commands
  *
- * @argv: argv argument
  * @input: input variable
+ * @argv: argv argument
  */
 void execute_command(char input[], char argv[])
 {
-	pid_t pid;
+    char *token;
+    char *saveptr;
 
-	/* Fork a child process */
-	pid = fork();
+    /* Tokenize the input based on ';' */
+    token = strtok_r(input, ";", &saveptr);
 
-	if (pid < 0)
-	{
-		perror(argv);
-		exit(1);
-	} else if (pid == 0)
-	{
-		/* In the child process */
-		char *args[64];
-		int i = 0;
+    while (token != NULL)
+    {
+        /* Fork a child process for each command */
+        pid_t pid = fork();
 
-		handle_path(input, args, i, argv);
-		/*
-		* If the loop completes,
-		* the command was not found in any directory in PATH
-		*/
-		perror(argv);
-		exit(1);
-	} else
-	{
-		/* In the parent process */
-		int status;
+        if (pid < 0)
+        {
+            perror(argv);
+            exit(1);
+        }
+        else if (pid == 0)
+        {
+            /* In the child process */
+            char *args[64];
+            int i = 0;
 
-		waitpid(pid, &status, 0);
-	}
+            handle_path(token, args, i, argv);
+
+            /* If the loop completes, the command was not found in any directory in PATH */
+            perror(argv);
+            exit(1);
+        }
+        else
+        {
+            /* In the parent process */
+            int status;
+            waitpid(pid, &status, 0);
+        }
+
+        /* Get the next command */
+        token = strtok_r(NULL, ";", &saveptr);
+    }
 }
+
