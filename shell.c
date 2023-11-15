3b90d87855fb4a5750d046ef27487f8bcd0d6e7b
samuel obunge <samuelobunge@gmail.com>
2023-11-15 07:57:19 -0800

    change

--- shell.c
+++ shell.c
@@ -1,23 +1,98 @@
 #include "main.h"
 
-int main(void) {
-    char command[100];
+/**
+ * handle_input - handle input function
+ *
+ * @command: command argument
+ */
+void handle_input(char *command)
+{
+	/* Fork a child process */
+	pid_t pid = fork();
 
-    while (1) {
-        /* Display the prompt */
-        printf("#cisfun$ ");
-        fflush(stdout);
+	if (pid < 0)
+	{
+		perror("./shell");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		/*
+		 * In the child process
+		 * Execute the command using execve
+		 */
+		char *args[1];
+		args[0] = command;
+		args[1] = NULL;
 
-        /* Read the command from the user */
-        if (fgets(command, sizeof(command), stdin) == NULL) {
-            /* Handle end of file (Ctrl+D or similar) */
-            break;
-        }
+		if (execve(command, args, environ) == -1)
+		{
+			/* If execve fails, print an error message */
+			perror("./shell");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		/* In the parent process */
+		int status;
 
-        /* Remove the newline character from the command */
-        command[strcspn(command, "\n")] = '\0';
-
-    }
-
-    return 0;
+		waitpid(pid, &status, 0);
+	}
 }
+
+/**
+ * is_whitespace - is white space function
+ *
+ * @str: command
+ */
+int is_whitespace(const char *str)
+{
+	while (*str != '\0')
+	{
+		if (!isspace((unsigned char)*str))
+		{
+			return 0;
+		}
+		str++;
+	}
+	return 1;
+}
+
+/**
+ * main - main function
+ * Return:
+ * - Returns 0 upon successful execution.
+ */
+int main(void)
+{
+	char command[100];
+
+	while (1)
+	{
+		/* Display the prompt */
+		printf("#cisfun$ ");
+
+		if (fgets(command, sizeof(command), stdin) == NULL)
+		{
+			/* Handle end of file (Ctrl+D) */
+			printf("\n");
+			break;
+		}
+
+		/* Remove the newline character from the command */
+		command[strcspn(command, "\n")] = '\0';
+
+		if (command[0] == '\0' || is_whitespace(command))
+		{
+			continue;
+		}
+		else
+		{
+			handle_input(command);
+		}
+	}
+
+	return (0);
+}
+
